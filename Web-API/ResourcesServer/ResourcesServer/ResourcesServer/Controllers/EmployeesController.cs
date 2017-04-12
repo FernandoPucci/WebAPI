06a969e4385a7981e1e6601d547dbe850870636b
FernandoPucci <fsilvapucci@gmail.com>
2017-04-12 14:39:44 -0300

    Refatoração

--- Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/EmployeesController.cs
+++ Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/EmployeesController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Web.Http;
 using ResourcesServer.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace ResourcesServer.Controllers
--- Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Models/Employee.cs
+++ Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Models/Employee.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace ResourcesServer.Models
 {
