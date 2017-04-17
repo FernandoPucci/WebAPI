15c732a209ed015ed82d8a693975a15b86d5fa35
FernandoPucci <fsilvapucci@gmail.com>
2017-04-17 11:19:52 -0300

    Atualização dos HTTP STATUS

--- Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/EmployeesController.cs
+++ Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/EmployeesController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.Web.Http;
+using ResourcesServer.Helpers;
 using ResourcesServer.Models;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace ResourcesServer.Controllers
 {
     [Authorize]
     [ApiVersion("1.0")]
     [ApiVersion("2.0")]
-    //[Route("api/v{version:apiVersion}/[controller]")]
     [RoutePrefix("api/v{version:apiVersion}/Employees")]
     public class EmployeesController : ApiController
     {
@@ -28,60 +31,103 @@ namespace ResourcesServer.Controllers
 
         [HttpGet]
         [Route]
-        public IQueryable<Employee> Get()
+        [ResponseType(typeof(IQueryable<Employee>))]
+        public HttpResponseMessage Get(HttpRequestMessage request)
         {
 
             var identity = User.Identity as ClaimsIdentity;
 
-            foreach (Claim c in identity.Claims) {
-                Debug.WriteLine(c.Type + " >> " + c.Value);
+            if (!AuthenticationHelper.GetAdministratorPermission(identity))
+            {
+                return request.CreateResponse(HttpStatusCode.Unauthorized, "Necessária Permissão de Administrator.");
+            };
 
-            }
-
-            return db.Employees.AsQueryable();
+            return request.CreateResponse(HttpStatusCode.OK, db.Employees.AsQueryable());
 
         }
 
         [HttpGet]
         [Route("{key}")]
-        public Employee Get(int key)
+        [ResponseType(typeof(Employee))]
+        public HttpResponseMessage Get(HttpRequestMessage request, int key)
         {
             IQueryable<Employee> result = db.Employees.Where(p => p.EmpNo == key);
-            return result.FirstOrDefault();
+
+            if (result == null || result.Count() == 0) {
+                return request.CreateResponse(HttpStatusCode.NoContent);
+            }
+
+            return request.CreateResponse(HttpStatusCode.OK, result.FirstOrDefault());
         }
 
 
         //V2
         [HttpGet, MapToApiVersion("2.0")] //Map this method to next version in the same controller     
         [Route("{key}")]
-        public SingleResult<Employee> GetV2(int key) //Rename the method
+        [ResponseType(typeof(SingleResult<Employee>))]
+        public HttpResponseMessage GetV2(HttpRequestMessage request, int key) //Rename the method
         {
             IQueryable<Employee> result = db.Employees.Where(p => p.EmpNo == key);
-            return SingleResult.Create(result);
+
+            if (result == null || result.Count() == 0)
+            {
+                return request.CreateResponse(HttpStatusCode.NoContent);
+            }
+
+            return request.CreateResponse(HttpStatusCode.OK, SingleResult.Create(result)); //difference between both methods,
+                                                                                           //only is the type of return 
+                                                                                           //(Employee Object @v1 vs. IQeryable @v2)
         }
-
-
+        
         [HttpPost]
         [Route]
         public async Task<IHttpActionResult> Post(Employee employee)
         {
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+
+            var identity = User.Identity as ClaimsIdentity;
+
+            //ahother way, throwing an Exception
+            if (!AuthenticationHelper.GetAdministratorPermission(identity))
+            {
+                //using an Exception Text
+                //throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized) { ReasonPhrase = "Necessária Permissão de Administrator." });
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            };
+
             db.Employees.Add(employee);
             await db.SaveChangesAsync();
+
             return Created("", employee);
         }
 
         //TODO:
-        public void Put(int id, string value)
+        [HttpPut]
+        [Route("{key}/{value}")]
+        public HttpResponseMessage Put(HttpRequestMessage request, int key, string value)
         {
+            return request.CreateResponse(HttpStatusCode.MethodNotAllowed);
         }
-        
-        public void Delete(int id)
+
+        [HttpDelete]
+        [Route("{key}")]
+        public HttpResponseMessage Delete(HttpRequestMessage request, int key)
         {
+            return request.CreateResponse(HttpStatusCode.MethodNotAllowed);
         }
+
+        [HttpPatch]
+        [Route("{key}")]
+        public HttpResponseMessage Patch(HttpRequestMessage request, int key)
+        {
+            return request.CreateResponse(HttpStatusCode.MethodNotAllowed);
+        }
+
+      
 
     }
 }
---
+++ Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace ResourcesServer.Helpers
+{
+    public class AuthenticationHelper
+    {
+
+        public const string ADMINISTRATOR_GRANT = "ADMINISTRATOR"; 
+
+        public static bool GetAdministratorPermission(ClaimsIdentity claims)
+        {
+
+            //only for debug purposes
+            Debug.WriteLine("++ " + claims.FindFirst("userName").Value);
+            Debug.WriteLine("++ " + claims.FindFirst("name").Value);
+            Debug.WriteLine("++ " + claims.FindFirst("permissionRole").Value);
+            Debug.WriteLine("++ " + claims.FindFirst("creationDate").Value);
+
+            return claims.FindFirst("permissionRole").Value == ADMINISTRATOR_GRANT;
+
+        }
+
+    }
+}
