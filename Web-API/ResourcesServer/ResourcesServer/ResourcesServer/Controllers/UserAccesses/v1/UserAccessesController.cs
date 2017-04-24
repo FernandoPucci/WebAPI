71ad29784d939d17ee1b70388afda9a2fe085e98
FernandoPucci <fsilvapucci@gmail.com>
2017-04-24 11:54:39 -0300

    Adição de paginação

--- Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/UserAccesses/v1/UserAccessesController.cs
+++ Web-API/ResourcesServer/ResourcesServer/ResourcesServer/Controllers/UserAccesses/v1/UserAccessesController.cs
@@ -26,24 +26,47 @@ namespace ResourcesServer.Controllers.UserAccess
         #region Default Verbs
 
         /// <summary>
-        /// Get a list of All UserAccesses (Need Authentication) (Only for ADMINISTRATOR roles) V1.0
+        /// <summary> Get a list of All UserAccesses (Need Authentication) (Only for ADMINISTRATOR roles) V1.0
         /// </summary>
         /// <param name="request"></param>
-        /// <returns>A list o Accesses</returns>
+        /// <param name="_offset">Initial offset</param>
+        /// <param name="_limit">Maximum results per page</param>
+        /// <returns></returns>
         [HttpGet]
         [Route]
         [ResponseType(typeof(IQueryable<Models.UserAccess>))]
-        public HttpResponseMessage Get(HttpRequestMessage request)
+        public HttpResponseMessage Get(HttpRequestMessage request, int _offset = 0, int _limit = 5)
         {
 
             var identity = User.Identity as ClaimsIdentity;
 
-            if (!AuthenticationHelper.GetAdministratorPermission(identity, "GET " + CONTROLLER_V1))
+            if (!AuthenticationHelper.GetAdministratorPermission(identity, "GET " + CONTROLLER_V1 + ((_offset != 0 || _limit != 5) ? " Called W/ Pagination: " + _offset + " to " + _limit : "")))
             {
                 return request.CreateResponse(HttpStatusCode.Unauthorized, "Necessária Permissão de Administrator.");
             };
 
-            return request.CreateResponse(HttpStatusCode.OK, db.UserAccesses.AsQueryable().OrderByDescending(g => g.AccessDate));
+            var total = db.UserAccesses.Count();
+            // var userAccesses = db.UserAccesses.AsQueryable().OrderByDescending(g => g.AccessDate);
+            var userAccesses = db.UserAccesses
+                .OrderByDescending(g => g.AccessDate)
+                .Skip(_offset)
+                .Take(_limit)
+                .AsQueryable();
+
+            return request.CreateResponse(HttpStatusCode.OK,
+                new
+                {
+                    Data = userAccesses,
+                    Paging = new
+                    {
+                        Total = total,
+                        Limit = _limit,
+                        Offset = _offset,
+                        Returned = userAccesses.Count()
+
+                    }
+                }
+                );
 
 
         }
